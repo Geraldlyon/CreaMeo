7161c84ddc1a0287396100c333e1398cb1ac2aed
Geraldlyon <geraldlyonga@gmail.com>
2021-08-15 22:43:14 +0100

    first commit of site

---
+++ js/app.js
@@ -0,0 +1,45 @@
+$(document).ready(function(){
+
+    // Product Carousel
+    $('#product-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        dots:false,
+        autoplay:true,
+        smartSpeed:1000,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:2
+            },
+            1000:{
+                items:4
+            }
+        }
+    })
+
+        // Testimonial Carousel
+        $('#testimonial-carousel').owlCarousel({
+            loop:true,
+            margin:10,
+            nav:true,
+            dots:false,
+            autoplay:true,
+            smartSpeed:1000,
+            responsive:{
+                0:{
+                    items:1
+                },
+                600:{
+                    items:1
+                },
+                1000:{
+                    items:2
+                }
+            }
+        })
+
+});
